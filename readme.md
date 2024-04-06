4a26b2118e6cf130f815b64570148d0dc906642a
KeithGrennier <keith.grennier@gmail.com>
2024-04-06 18:13:30 -0500

    Created first get RESTAPI, getAlbums

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Tutorial
+Link: [web-service-gin](https://go.dev/doc/tutorial/web-service-gin 'Restful Tutorial')
+# Getting Started
+git clone /reponame/
+# Using this
+curl localhost:8080/albums
--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,7 +1,13 @@
 package main
 
-//_standalone package rather than a library
-//_Store album data in memory
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// _standalone package rather than a library
+// _Store album data in memory
 // album data about record album.
 type album struct {
 	ID     string  `json:"id"`
@@ -15,4 +21,16 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
+
+// getAlbums responds with list of all albums as json
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
